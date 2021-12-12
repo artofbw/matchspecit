0aa61ad12dcbf28e513fbede3627477e96ca5f0f
Łukasz Ćwikliński <artofbw@gmail.com>
2021-12-12 21:13:49 +0100

    boilerplate

---
+++ backend/settings_test.py
@@ -0,0 +1,21 @@
+# flake8: noqa
+from backend.settings_local import *
+
+DEBUG = False
+SSL_ENABLED = False
+
+DEBUG_TOOLBAR_CONFIG = {
+    # don't show Debug Toolbar in tests
+    'SHOW_TOOLBAR_CALLBACK': lambda request: False,
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'test.db'),
+        },
+    },
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
