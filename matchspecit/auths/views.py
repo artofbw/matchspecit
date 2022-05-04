15c2ef260e6748aa4bc45fbfb36986034cd84494
Hubert Korzeniewski <hk190696@gmail.com>
2022-05-04 18:42:41 +0200

    poprawa

--- matchspecit/auths/views.py
+++ matchspecit/auths/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import get_template, render_to_string
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -7,6 +9,7 @@ from matchspecit.auths.serializers import (
     MyTokenObtainPairSerializer,
     RegisterSerializer,
 )
+from matchspecit.core import settings
 
 
 class ObtainTokenPairView(TokenObtainPairView):
@@ -15,6 +18,18 @@ class ObtainTokenPairView(TokenObtainPairView):
 
 
 class RegisterView(generics.CreateAPIView):
+    merge_data = {
+        'user': "user"
+    }
+    html_body = render_to_string("email/register_email_confirmation.html", merge_data)
+    message = EmailMultiAlternatives(
+       subject='Django HTML Email',
+       from_email=settings.EMAIL_HOST_USER,
+       to=[settings.EMAIL_HOST]
+    )
+    message.attach_alternative(html_body, "text/html")
+    message.send(fail_silently=False)
+
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
