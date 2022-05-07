650f32db50b4702c14ce098d0abeb75b39cb1433
Hubert Korzeniewski <47918301+vechten@users.noreply.github.com>
2022-05-07 14:56:43 +0200

    Restore views.py

--- matchspecit/auths/views.py
+++ matchspecit/auths/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.models import User
-from django.core.mail import send_mail, EmailMultiAlternatives
-from django.template.loader import get_template, render_to_string
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -9,7 +7,6 @@ from matchspecit.auths.serializers import (
     MyTokenObtainPairSerializer,
     RegisterSerializer,
 )
-from matchspecit.core import settings
 
 
 class ObtainTokenPairView(TokenObtainPairView):
