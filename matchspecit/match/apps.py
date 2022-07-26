652952c3a05913b5d947563a6ecc6cc069f609bb
Lukasz Cwiklinski <lukasz.cwiklinski@stxnext.pl>
2022-07-26 22:15:27 +0200

    aktualizacja naglowka aplikacji

--- matchspecit/match/apps.py
+++ matchspecit/match/apps.py
@@ -3,5 +3,4 @@ from django.apps import AppConfig
 
 class MatchConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'match'
-    verbose_name = 'pasowanie'
+    name = 'matchspecit.match'
