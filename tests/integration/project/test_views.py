56ab179c00f6e0a25e5057cd4d46701c838aadc8
levar <maciej.rybacki@playbookengineering.com>
2022-08-08 20:33:03 +0200

    Odkomentowano działające testy

--- tests/integration/project/test_views.py
+++ tests/integration/project/test_views.py
@@ -1,6 +1,7 @@
 import pytest
 from django.forms import model_to_dict
 from django.urls import reverse
+from rest_framework import status
 
 
 def project_to_dict(project):
@@ -54,28 +55,61 @@ class TestProjectView:
 class TestProjectDetail:
     url_namespace = "project_detail"
 
+    def test_project_detail_get_by_incorrect_id_return_not_found(self, api_client, create_user, test_password):
+        url = reverse(self.url_namespace, args=[9999])
+        user = create_user()
+        api_client.force_authenticate(user=user)
+
+        response = api_client.get(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert response.json() == {"detail": "Not found."}
+
+    def test_project_detail_delete_by_incorrect_id_return_not_found(self, api_client, create_user, test_password):
+        url = reverse(self.url_namespace, args=[9999])
+        user = create_user()
+        api_client.force_authenticate(user=user)
+
+        response = api_client.put(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert response.json() == {"detail": "Not found."}
+
+
+    def test_project_detail_put_by_incorrect_id_return_not_found(self, api_client, create_user, test_password):
+        url = reverse(self.url_namespace, args=[9999])
+        user = create_user()
+        api_client.force_authenticate(user=user)
+
+        response = api_client.put(url)
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert response.json() == {"detail": "Not found."}
+
+
+"""    
+    def test_project_detail_get_by_id_return_project(self, api_client, create_user, test_password, create_project):
+        project = create_project()
+        url = reverse(self.url_namespace, args=[project.id])
+        user = create_user()
+        api_client.force_authenticate(user=user)
+
+        response = api_client.get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == project_to_dict(project)
+"""
 
 """
-       def test_project_detail_get_by_id_return_project(self, api_client, create_user, test_password, create_project):
-       project = create_project()
-       url = reverse(self.url_namespace, args=[project.id])
-       user = create_user()
-       api_client.force_authenticate(user=user)
-       response = api_client.get(url)
-       assert response.status_code == status.HTTP_200_OK
-       assert response.json() == project_to_dict(project)
-"""
+    def test_project_detail_delete_by_id_removes_project(self, api_client, create_user, test_password, create_project):
+        project = create_project()
+        url = reverse(self.url_namespace, args=[project.id])
+        user = create_user()
+        api_client.force_authenticate(user=user)
 
-"""
-def test_project_detail_get_by_incorrect_id_return_not_found(self, api_client, create_user, test_password):
-    url = reverse(self.url_namespace, args=[9999])
-    user = create_user()
-    api_client.force_authenticate(user=user)
+        response = api_client.delete(url)
 
-    response = api_client.get(url)
-
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == {"detail": "Not found."}
+        assert response.status_code == status.HTTP_204_NO_CONTENT
 """
 
 """
@@ -84,44 +118,10 @@ def test_project_detail_get_by_incorrect_id_return_not_found(self, api_client, c
         url = reverse(self.url_namespace, args=[project.id])
         user = create_user()
         api_client.force_authenticate(user=user)
-
+    
         response = api_client.put(url, data={"name": "test updated name"})
         project.refresh_from_db()
-
+    
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == project_to_dict(project)
-"""
-
-"""
-def test_project_detail_put_by_incorrect_id_return_not_found(self, api_client, create_user, test_password):
-    url = reverse(self.url_namespace, args=[9999])
-    user = create_user()
-    api_client.force_authenticate(user=user)
-
-    response = api_client.put(url)
-
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == {"detail": "Not found."}
-
-"""
-"""
-   def test_project_detail_delete_by_id_removes_project(self, api_client, create_user, test_password, create_project):
-       project = create_project()
-       url = reverse(self.url_namespace, args=[project.id])
-       user = create_user()
-       api_client.force_authenticate(user=user)
-       response = api_client.delete(url)
-       assert response.status_code == status.HTTP_204_NO_CONTENT
-"""
-
-"""
-def test_project_detail_delete_by_incorrect_id_return_not_found(self, api_client, create_user, test_password):
-    url = reverse(self.url_namespace, args=[9999])
-    user = create_user()
-    api_client.force_authenticate(user=user)
-
-    response = api_client.put(url)
-
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == {"detail": "Not found."}
 """
