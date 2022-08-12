bde29f77d164f02f057ec691dbaf05cae1a05541
Lukasz Cwiklinski <lukasz.cwiklinski@stxnext.pl>
2022-08-12 23:53:56 +0200

    dopasowanie - celery

--- matchspecit/match/tasks.py
+++ matchspecit/match/tasks.py
@@ -3,26 +3,32 @@ from decimal import Decimal
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-from matchspecit.core.celery import shared_task
+from celery import shared_task
+from psycopg2 import IntegrityError
+
 from matchspecit.match.models import Match
 from matchspecit.project.models import Project
-from matchspecit.user.models import Role
 
 User = get_user_model()
 
 
 @shared_task
 def match_user_with_project():
-    users = User.objects.filter(is_active=True, role=Role.SPECIALIST)
-    projects = Project.objects.all()
+    users = User.objects.filter(is_active=True, is_matchable=True)
+    projects = Project.objects.filter(is_matchable=True, is_finish=False, is_successful=False, is_deleted=False)
 
     for user in users:
         for project in projects:
-            user_technologies = user.technologies.count()
-            project_technologies = project.technologies.count()
+            if user.id != project.owner_id:
+                user_technologies = user.technologies.count()
+                project_technologies = project.technologies.count()
 
-            match_percent = round(Decimal(user_technologies) / Decimal(project_technologies), 2)
+                match_percent = round(Decimal(user_technologies) / Decimal(project_technologies), 2)
 
-            if match_percent >= settings.MATCH_PERCENT:
-                Match.objects.create(user=user, project=project, match_percent=match_percent)
+                if match_percent >= settings.MATCH_PERCENT:
+                    try:
+                        Match.objects.create(user=user, project=project, match_percent=match_percent)
+                        return {"status": "matched"}
+                    except IntegrityError:
+                        pass
     return None
