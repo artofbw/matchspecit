b6c0b1850100760ae9863468ff64a1eec094c22f
levar <maciej.rybacki@playbookengineering.com>
2022-08-13 22:39:32 +0200

    Usunięto nieużywany import

--- matchspecit/project/views.py
+++ matchspecit/project/views.py
@@ -2,7 +2,6 @@ from django.http import Http404
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import permissions, status
-from rest_framework.parsers import MultiPartParser
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
