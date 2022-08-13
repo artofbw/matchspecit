49314c6b1de45f696133f9851972059561017d3b
levar <maciej.rybacki@playbookengineering.com>
2022-08-13 19:23:24 +0200

    Poprawiono formatowanie

--- matchspecit/project/serializers.py
+++ matchspecit/project/serializers.py
@@ -1,8 +1,7 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from matchspecit.project.models import Project
-
-from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
