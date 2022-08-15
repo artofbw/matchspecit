b8e52e3370e3c9236d9ce3fd61f1076f529d28b5
Hubert Korzeniewski <hk190696@gmail.com>
2022-08-15 20:36:00 +0200

    url do frontu

--- matchspecit/auths/signals.py
+++ matchspecit/auths/signals.py
@@ -23,7 +23,7 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
         "username": reset_password_token.user.username,
         "email": reset_password_token.user.email,
         "reset_password_url": "{}?token={}".format(
-            instance.request.build_absolute_uri(reverse("password_reset:reset-password-confirm")),
+            "http://localhost:3000/reset-password",
             reset_password_token.key,
         ),
     }
