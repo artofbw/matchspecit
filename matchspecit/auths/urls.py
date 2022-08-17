01a42ce9350e878200a24c5f5ee41b3d4d72d7af
levar <maciej.rybacki@playbookengineering.com>
2022-08-17 22:24:06 +0200

    Zmiana hasła

--- matchspecit/auths/urls.py
+++ matchspecit/auths/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from matchspecit.auths.views import ObtainTokenPairView, RegisterView
+from matchspecit.auths.views import ObtainTokenPairView, RegisterView, ChangePasswordView
 
 urlpatterns = [
     path("login/", ObtainTokenPairView.as_view(), name="token_obtain_pair"),
     path("login/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", RegisterView.as_view(), name="auth_register"),
     path(r"password_reset/", include("django_rest_passwordreset.urls", namespace="password_reset")),
+    path('change_password/<int:pk>/', ChangePasswordView.as_view(), name='auth_change_password'),
 ]
--- matchspecit/auths/views.py
+++ matchspecit/auths/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from matchspecit.auths.serializers import (
     MyTokenObtainPairSerializer,
-    RegisterSerializer,
+    RegisterSerializer, ChangePasswordSerializer,
 )
 
 User = get_user_model()
@@ -20,3 +20,9 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class ChangePasswordView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ChangePasswordSerializer
