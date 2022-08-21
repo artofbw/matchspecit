7c8faa016e8cd9e47b223aa22fbc5548652817dd
Hubert Korzeniewski <hk190696@gmail.com>
2022-08-21 20:46:55 +0200

    add deleted code

--- matchspecit/auths/urls.py
+++ matchspecit/auths/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="auth_register"),
     path(r"password_reset/", include("django_rest_passwordreset.urls", namespace="password_reset")),
     path("email-verify/", VerifyEmail.as_view(), name="email-verify"),
+    path("change_password/<int:pk>/", ChangePasswordView.as_view(), name="auth_change_password"),
 ]
